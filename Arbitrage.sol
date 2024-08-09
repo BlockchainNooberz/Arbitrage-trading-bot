e04486e969fc4f49bc87a49f2863452bf41ad139
BlockchainNooberz <andrewelston177@gmail.com>
2024-08-09 11:52:40 -0700

    Update Arbitrage.sol

--- Arbitrage.sol
+++ Arbitrage.sol
@@ -1,130 +1,70 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.18;
+const ethers = require("ethers")
+const Big = require('big.js')
+const IUniswapV2Pair = require("@uniswap/v2-core/build/IUniswapV2Pair.json")
+const IERC20 = require('@openzeppelin/contracts/build/contracts/ERC20.json')
 
-import "@balancer-labs/v2-interfaces/contracts/vault/IVault.sol";
-import "@balancer-labs/v2-interfaces/contracts/vault/IFlashLoanRecipient.sol";
-import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+async function getTokenAndContract(_token0Address, _token1Address, _provider) {
+    const token0Contract = new ethers.Contract(_token0Address, IERC20.abi, _provider)
+    const token1Contract = new ethers.Contract(_token1Address, IERC20.abi, _provider)
 
-contract Arbitrage is IFlashLoanRecipient {
-    IVault private constant vault =
-        IVault(0xBA12222222228d8Ba445958a75a0704d566BF2C8);
-
-    IUniswapV2Router02 public immutable sRouter;
-    IUniswapV2Router02 public immutable uRouter;
-    address public owner;
-
-    constructor(address _sRouter, address _uRouter) {
-        sRouter = IUniswapV2Router02(_sRouter); // Sushiswap
-        uRouter = IUniswapV2Router02(_uRouter); // Uniswap
-        owner = msg.sender;
+    const token0 = {
+        address: _token0Address,
+        decimals: 18,
+        symbol: await token0Contract.symbol(),
+        name: await token0Contract.name()
     }
 
-    function executeTrade(
-        bool _startOnUniswap,
-        address _token0,
-        address _token1,
-        uint256 _flashAmount
-    ) external {
-        bytes memory data = abi.encode(_startOnUniswap, _token0, _token1);
-
-        // Token to flash loan, by default we are flash loaning 1 token.
-        IERC20[] memory tokens = new IERC20[](1);
-        tokens[0] = IERC20(_token0);
-
-        // Flash loan amount.
-        uint256[] memory amounts = new uint256[](1);
-        amounts[0] = _flashAmount;
-
-        vault.flashLoan(this, tokens, amounts, data);
+    const token1 = {
+        address: _token1Address,
+        decimals: 18,
+        symbol: await token1Contract.symbol(),
+        name: await token1Contract.name()
     }
 
-    function receiveFlashLoan(
-        IERC20[] memory tokens,
-        uint256[] memory amounts,
-        uint256[] memory feeAmounts,
-        bytes memory userData
-    ) external override {
-        require(msg.sender == address(vault));
+    return { token0Contract, token1Contract, token0, token1 }
+}
 
-        uint256 flashAmount = amounts[0];
+async function getPairAddress(_V2Factory, _token0, _token1) {
+    const pairAddress = await _V2Factory.getPair(_token0, _token1)
+    return pairAddress
+}
 
-        (bool startOnUniswap, address token0, address token1) = abi.decode(
-            userData,
-            (bool, address, address)
-        );
+async function getPairContract(_V2Factory, _token0, _token1, _provider) {
+    const pairAddress = await getPairAddress(_V2Factory, _token0, _token1)
+    const pairContract = new ethers.Contract(pairAddress, IUniswapV2Pair.abi, _provider)
+    return pairContract
+}
 
-        // Use the money here!
-        address[] memory path = new address[](2);
+async function getReserves(_pairContract) {
+    const reserves = await _pairContract.getReserves()
+    return [reserves.reserve0, reserves.reserve1]
+}
 
-        path[0] = token0;
-        path[1] = token1;
+async function calculatePrice(_pairContract) {
+    const [x, y] = await getReserves(_pairContract)
+    return Big(x).div(Big(y))
+}
 
-        if (startOnUniswap) {
-            _swapOnUniswap(path, flashAmount, 0);
+async function calculateDifference(_uPrice, _sPrice) {
+    return (((_uPrice - _sPrice) / _sPrice) * 100).toFixed(2)
+}
 
-            path[0] = token1;
-            path[1] = token0;
+async function simulate(_amount, _routerPath, _token0, _token1) {
+    const trade1 = await _routerPath[0].getAmountsOut(_amount, [_token0.address, _token1.address])
+    const trade2 = await _routerPath[1].getAmountsOut(trade1[1], [_token1.address, _token0.address])
 
-            _swapOnSushiswap(
-                path,
-                IERC20(token1).balanceOf(address(this)),
-                flashAmount
-            );
-        } else {
-            _swapOnSushiswap(path, flashAmount, 0);
+    const amountIn = ethers.formatUnits(trade1[0], 'ether')
+    const amountOut = ethers.formatUnits(trade2[1], 'ether')
 
-            path[0] = token1;
-            path[1] = token0;
+    return { amountIn, amountOut }
+}
 
-            _swapOnUniswap(
-                path,
-                IERC20(token1).balanceOf(address(this)),
-                flashAmount
-            );
-        }
-
-        IERC20(token0).transfer(address(vault), flashAmount);
-
-        IERC20(token0).transfer(owner, IERC20(token0).balanceOf(address(this)));
-    }
-
-    // -- INTERNAL FUNCTIONS -- //
-
-    function _swapOnUniswap(
-        address[] memory _path,
-        uint256 _amountIn,
-        uint256 _amountOut
-    ) internal {
-        require(
-            IERC20(_path[0]).approve(address(uRouter), _amountIn),
-            "Uniswap approval failed."
-        );
-
-        uRouter.swapExactTokensForTokens(
-            _amountIn,
-            _amountOut,
-            _path,
-            address(this),
-            (block.timestamp + 1200)
-        );
-    }
-
-    function _swapOnSushiswap(
-        address[] memory _path,
-        uint256 _amountIn,
-        uint256 _amountOut
-    ) internal {
-        require(
-            IERC20(_path[0]).approve(address(sRouter), _amountIn),
-            "Sushiswap approval failed."
-        );
-
-        sRouter.swapExactTokensForTokens(
-            _amountIn,
-            _amountOut,
-            _path,
-            address(this),
-            (block.timestamp + 1200)
-        );
-    }
+module.exports = {
+    getTokenAndContract,
+    getPairAddress,
+    getPairContract,
+    getReserves,
+    calculatePrice,
+    calculateDifference,
+    simulate
 }
